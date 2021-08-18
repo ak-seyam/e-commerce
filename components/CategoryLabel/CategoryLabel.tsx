98b3d4c29bdf3867491db4b9771edbd772d98bc5
A-Siam <abdullah.khaled.kamal0@gmail.com>
2021-08-18 14:23:12 +0200

    fix: categories icons

--- components/CategoryLabel/CategoryLabel.tsx
+++ components/CategoryLabel/CategoryLabel.tsx
@@ -1,20 +1,27 @@
-import styles from "./categoy-label.module.css"
+import { IconDefinition } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import styles from "./categoy-label.module.css";
 
 /**
  * This is a low-level category label use it only if you really
  * want to use custom label
  */
 interface CategoryLabelProps {
-    text: string,
-    categoryClassName: string
+  text: string;
+  iconDefinition: IconDefinition;
 }
-const CategoryLabel: React.FC<CategoryLabelProps> = ({ categoryClassName, text }) => {
-    return <div className={styles["category-label"]}>
-        <div className={styles["icon-area"]}>
-            <i className={categoryClassName}></i>
-        </div>
-        <div className={`${styles["text-area"]}`}>{text}</div>
+const CategoryLabel: React.FC<CategoryLabelProps> = ({
+  iconDefinition,
+  text,
+}) => {
+  return (
+    <div className={styles["category-label"]}>
+      <div className={styles["icon-area"]}>
+        <FontAwesomeIcon icon={iconDefinition} />
+      </div>
+      <div className={`${styles["text-area"]}`}>{text}</div>
     </div>
-}
+  );
+};
 
 export default CategoryLabel;
--- components/CategoryLabel/CategoryLabelBuilder.tsx
+++ components/CategoryLabel/CategoryLabelBuilder.tsx
@@ -1,29 +1,69 @@
 import CategoryLabel from "./CategoryLabel";
+import {
+  faMobile,
+  faGlasses,
+  faShoePrints,
+  faTshirt,
+  faBaseballBall,
+  faTv,
+} from "@fortawesome/free-solid-svg-icons";
 
 const _categoryTag = {
-    MOBILE: "mobile",
-    CLOTHING: "clothing",
-    TV: "tv",
-    SHOES: "shoes",
-    ACCESSORIES: "accessories",
-    SPORTS: "sports"
-}
+  MOBILE: "mobile",
+  CLOTHING: "clothing",
+  TV: "tv",
+  SHOES: "shoes",
+  ACCESSORIES: "accessories",
+  SPORTS: "sports",
+};
 
 export const categoryTag = (category: string) => {
-    const catUp = category.toUpperCase()
-    const ct = _categoryTag[catUp]
-    return ct;
-}
+  const catUp = category.toUpperCase();
+  const ct = _categoryTag[catUp];
+  return ct;
+};
 
 const CategoryLabelMap = {
-    [_categoryTag.MOBILE]: <CategoryLabel key="mobile" text="Mobile" categoryClassName="fa fa-mobile" />,
-    [_categoryTag.ACCESSORIES]: <CategoryLabel key="access" text="Accessories" categoryClassName="fas fa-glasses" />,
-    [_categoryTag.SHOES]: <CategoryLabel key="shoes" text="Shoes" categoryClassName="fas fa-shoe-prints" />,
-    [_categoryTag.CLOTHING]: <CategoryLabel key="clothing" text="Clothing" categoryClassName="fas fa-tshirt" />,
-    [_categoryTag.SPORTS]: <CategoryLabel key="sports" text="Sports" categoryClassName="fas fa-baseball-ball" />,
-    [_categoryTag.TV]: <CategoryLabel key="tv" text="TV" categoryClassName="fas fa-tv" />,
-}
+  [_categoryTag.MOBILE]: (
+    <CategoryLabel
+      key="mobile"
+      text="Mobile"
+      iconDefinition={faMobile}
+    />
+  ),
+  [_categoryTag.ACCESSORIES]: (
+    <CategoryLabel
+      key="access"
+      text="Accessories"
+      iconDefinition={faGlasses}
+    />
+  ),
+  [_categoryTag.SHOES]: (
+    <CategoryLabel
+      key="shoes"
+      text="Shoes"
+      iconDefinition={faShoePrints}
+    />
+  ),
+  [_categoryTag.CLOTHING]: (
+    <CategoryLabel
+      key="clothing"
+      text="Clothing"
+      iconDefinition={faTshirt}
+    />
+  ),
+  [_categoryTag.SPORTS]: (
+    <CategoryLabel
+      key="sports"
+      text="Sports"
+      iconDefinition={faBaseballBall}
+    />
+  ),
+  [_categoryTag.TV]: (
+    <CategoryLabel key="tv" text="TV" iconDefinition={faTv} />
+  ),
+};
 
 export const CategoryLabelBuilder = (category: string): JSX.Element => {
-    return CategoryLabelMap[categoryTag(category)];
-}
+  return CategoryLabelMap[categoryTag(category)];
+};
