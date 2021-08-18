8cfcd0a1bc85cb4acc2ea79ab00aa137665b5bca
A-Siam <abdullah.khaled.kamal0@gmail.com>
2021-08-18 14:23:05 +0200

    fix baseUrl

--- axios-config/axios-config.ts
+++ axios-config/axios-config.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL: "https://asiam-e-commerce-be.herokuapp.com",
 });
 
 export default instance;
